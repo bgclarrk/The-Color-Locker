cf617e7ffbad0a68a166f9eb499cc3e1ae23eb21
+ <+>
2021-07-10 10:22:08 -0700

    Creating user table and model

---
+++ db/migrate/20210710171722_create_users_table.rb
@@ -0,0 +1,11 @@
+class CreateUsersTable < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :username
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
