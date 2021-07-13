8f9ade8422c0030849033b52a501c35c4e2841b0
+ <+>
2021-07-12 20:11:07 -0700

    Fixing issue on the show page and refactor controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+    before_action :find_project, only: [:index, :edit, :show, :destroy]
 
     def index
         @user = User.find(session[:user_id]) # Need to abstract out
@@ -24,15 +25,19 @@ class ProjectsController < ApplicationController
     def show
 
     end
-
+    
     def destroy
-
+        
     end
-
+    
     private
-
+    
     def project_params
         params.require(:projects).permit(:title, :description, :user_id)
+    end
+    
+    def find_project
+        @project = Project.find_by(params[:id])
     end
 
 end
